670b1d5a9da91dd27341549e63dd59d09b6a86e1
Prethor-byte <81958182+Prethor-byte@users.noreply.github.com>
2022-03-06 13:35:37 +0100

    second commit

--- main.py
+++ main.py
@@ -1,4 +1,19 @@
 from selenium import webdriver
+import time
+import os
 
+driver = webdriver.Chrome(r"/Praca/python/selenium_drivers/chromedriver.exe")
 
-driver = webdriver.Chrome(r"C:/Praca/python/selenium_drivers")
+driver.get('http://www.google.com/')
+
+time.sleep(5) # Let the user actually see something!
+
+search_box = driver.find_element_by_name('q')
+
+search_box.send_keys('ChromeDriver')
+
+search_box.submit()
+
+time.sleep(5) # Let the user actually see something!
+
+driver.quit()
